effc8c28b2a700090169bf554d351024731fc915
Albert Aghababyan <albertaghababyan@Alberts-Mac-mini.local>
2025-12-28 12:54:16 +0400

    first commit

---
+++ lib/screens/coffee_house_main_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CoffeeHouseMainScreen extends StatefulWidget {
+  const CoffeeHouseMainScreen({super.key});
+
+  @override
+  State<CoffeeHouseMainScreen> createState() => _CoffeeHouseMainScreenState();
+}
+
+class _CoffeeHouseMainScreenState extends State<CoffeeHouseMainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
